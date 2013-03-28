93f787749f24265131c6dee637ceefd1b0ade2c4
Chris Gansen <cgansen@barackobama.com>
2013-03-27 22:46:21 -0500

    rails new logan

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Logan::Application.config.secret_key_base = '610b1ed68608ed007db8af1cb317d93b735c906f5e036873d8ec30092f782e290d727b9072c40568dbc52c4397059e3e03fb9166be7cd3d01f03cc3c022e61fa'
