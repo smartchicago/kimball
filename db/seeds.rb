865087616295725573eb178c1b3f58cc720f30de
Chris Gansen <cgansen@barackobama.com>
2013-03-27 23:45:26 -0500

    adding seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Make some dummy people
+
+Person.create(
+  first_name: "Jim",
+  last_name: "Tester",
+  email_address: "jim@example.com",
+  address_1: "123 N Michigan Ave",
+  city: "Chicago",
+  state: "IL",
+  postal_code: "60601",
+  geography_id: "42", # ward
+  primary_device_id: 1,
+  primary_device_description: "Apple Macbook Pro",
+  secondary_device_id: 2,
+  secondary_device_description: "Samsung Galaxy",
+  primary_connection_id: 1,
+  phone_number: "312-555-9090",
+  participation_type: "in-person"
+)
+
+Person.create(
+  first_name: "Jane",
+  last_name: "Developer",
+  email_address: "jane@example.com",
+  address_1: "1060 W Addison",
+  city: "Chicago",
+  state: "IL",
+  postal_code: "60613",
+  geography_id: "44", # ward
+  primary_device_id: 1,
+  primary_device_description: "iPad",
+  secondary_device_id: 2,
+  secondary_device_description: "Apple laptop",
+  primary_connection_id: 1,
+  phone_number: "312-555-8888",
+  participation_type: "remote"
+)
