6b10be9ab8a64cee5b34d4ec836eb95b50f5f9f7
cgansen <cgansen@gmail.com>
2013-03-29 18:09:27 -0500

    updating people#show view

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -1,2 +1,6 @@
 module PeopleHelper
+  def address_fields_to_sentence(person)
+    [person.address_1, person.address_2, person.city, person.state, person.postal_code].reject{|i| i.blank? }.join(", ")
+  end
+
 end
