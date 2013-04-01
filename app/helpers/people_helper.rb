5883cfa47bcde391c67cfb54d21bf96e467fc7cb
cgansen <cgansen@gmail.com>
2013-03-31 22:30:11 -0500

    updating show, adding human values for device and connection type

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -3,4 +3,18 @@ module PeopleHelper
     [person.address_1, person.address_2, person.city, person.state, person.postal_code].reject{|i| i.blank? }.join(", ")
   end
 
+  def human_device_type_name(device_id)
+    Logan::Application.config.device_mappings.rassoc(device_id)[0].to_s
+  end
+
+  def human_connection_type_name(connection_id)
+    mappings = {:phone => "Phone with data plan", 
+      :home_broadband => "Home broadband (cable, DSL)", 
+      :other => "Other", 
+      :public_computer => "Public computer", 
+      :public_wifi => "Public wifi"
+    }
+    
+    mappings[Logan::Application.config.connection_mappings.rassoc(connection_id)[0]]
+  end
 end
