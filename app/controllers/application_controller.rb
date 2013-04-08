fbbb021c5390388e5f34e242a9d4fa1bc3a3092a
Chris Gansen <cgansen@barackobama.com>
2013-04-08 00:19:22 -0500

    adding janky http basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :janky_authentication
+
+  protected
+
+  def janky_authentication
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "logan" && password == "smartchicago!"
+    end
+  end
+
 end
