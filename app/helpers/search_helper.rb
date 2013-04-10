7c0c608f6ba9d776fb6ae1486aa1e50b75a1c4b5
cgansen <cgansen@gmail.com>
2013-04-10 17:52:54 -0500

    highlight matches in search results

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,2 +1,10 @@
 module SearchHelper
+
+  def search_result_field(value, search_facet = nil)
+    Rails.logger.debug("search_result_field: \n\tvalue:#{value}\n\tsearch_facet:#{search_facet}\n\tparams[:#{search_facet}]:#{params[search_facet]}")
+    # given a value and an optional search facet, highlight the value in the string
+    terms = [params[:q].to_s, params[search_facet].to_s].compact.delete_if{|a| a.blank? }
+    Rails.logger.debug("\tterms: #{terms}")
+    terms.any? ? highlight(value.to_s, terms) : value
+  end
 end
