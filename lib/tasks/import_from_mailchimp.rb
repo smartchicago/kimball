0bbc4ed3dd5c04476bb9362c771a43c250f786ba
cgansen <cgansen@gmail.com>
2013-04-10 16:05:53 -0500

    use the included methods in the Person method, formatting tweaks

--- lib/tasks/import_from_mailchimp.rb
+++ lib/tasks/import_from_mailchimp.rb
@@ -20,8 +20,7 @@ class MailChimpImporter
 
       person = Person.new
       column_map = {
-        "Email Address" => :email_address,"First Name" => :first_name,"Last Name" => :last_name,"Ward" => :geography_id,
-        "ZIP Code" => :postal_code,
+        "Email Address" => :email_address,"First Name" => :first_name,"Last Name" => :last_name,"Ward" => :geography_id, "ZIP Code" => :postal_code,
         "How would you like to participate" => :participation_type, "Primary device" => :primary_device_id,"Primary device description" => :primary_device_description,
         "Secondary device" => :secondary_device_id, "Secondary device description" => :secondary_device_description ,"Primary connection method" => :primary_connection_id,
         "Connection method description" => :primary_connection_description, "Did you vote in the most recent election" => :voted, "Have you ever called 311" => :called_311
@@ -29,15 +28,15 @@ class MailChimpImporter
       
       column_map.each do |k,v|
         person[v] = if k == "Primary device" || k == "Secondary device" 
-            map_device_to_id(line[k])
+            Person.map_device_to_id(line[k])
           elsif k == "Primary connection method"
-            map_connection_to_id(line[k])
+            Person.map_connection_to_id(line[k])
           else 
             line[k]
           end
       end
       
-      Rails.logger.info "[load_mailchimp_csv] saving a new person: #{line[:email_address]}"
+      Rails.logger.info "[load_mailchimp_csv] saving a new person: #{line['Email']}"
             
       person.save
       $stdout.print "."
@@ -74,48 +73,12 @@ class MailChimpImporter
     $stdout.puts "\ncompleted wufoo import, #{count} records"
     
   end
-
-  def map_connection_to_id(val)
-    sym = case val
-    when "Broadband at home (cable, DSL, etc.)", "Broadband at home (e.g. cable or DSL)"
-      :home_broadband
-    when "Public computer center"
-      :public_computer
-    when "Phone plan with data"
-      :phone
-    when "Public wi-fi", "Public Wi-Fi"
-      :public_wifi
-    when "Other"
-      :other
-    end
-
-    Logan::Application.config.connection_mappings[sym]    
-  end
-  
-  def map_device_to_id(val)
-      sym = case val
-      when "Laptop"
-        :laptop
-      when "Smart phone", "Smart phone (e.g. iPhone or Android phone)"
-        :smartphone
-      when "Desktop computer"
-        :desktop
-      when "Tablet", "Tablet (e.g. iPad)"
-        :tablet
-      end
-      
-      Logan::Application.config.device_mappings[sym]
-  end
   
   def run
     load_mailchimp_csv
     load_wufoo_csv
-  end
-  
+  end  
 end
-
-
-###
 
 options = OpenStruct.new
 OptionParser.new do |opts|
