2f4264ec85b31805c6bb15744dd2fc6c3e018b3f
Chris Gansen <cgansen@barackobama.com>
2013-05-01 22:37:30 -0500

    fix pagination of adv search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,12 +3,12 @@ require 'csv'
 class SearchController < ApplicationController
   def index
     # no pagination for CSV export
-    per_page = request.format.to_s.eql?('text/csv') ? nil : Person.per_page
+    per_page = request.format.to_s.eql?('text/csv') ? 10000 : Person.per_page
     
     @results = if params[:q]
       Person.search params[:q], per_page: per_page, page: (params[:page] || 1)
     elsif params[:adv]
-      Person.complex_search(params, 10000) # FIXME: more elegant solution for returning all records
+      Person.complex_search(params, per_page) # FIXME: more elegant solution for returning all records
     else
       []
     end    
