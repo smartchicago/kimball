8118b641c99514146a700f1b24214f8ce477c77d
Chris Gansen <cgansen@barackobama.com>
2013-05-03 11:35:57 -0500

    wip:fleshing out user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130423195105) do
+ActiveRecord::Schema.define(version: 20130502205047) do
 
   create_table "comments", force: true do |t|
     t.text     "content"
@@ -46,6 +46,23 @@ ActiveRecord::Schema.define(version: 20130423195105) do
     t.datetime "signup_at"
     t.string   "voted"
     t.string   "called_311"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "password_salt"
+    t.string   "invitation_token"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
