9e37bae8cf2932caee1e608961a07acc9208aa03
cgansen <cgansen@gmail.com>
2013-05-06 16:42:16 -0500

    use https for session cookie

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Logan::Application.config.session_store :encrypted_cookie_store, key: '_logan_session'
+Logan::Application.config.session_store :encrypted_cookie_store, key: '_logan_session', secure: (Rails.env.production? || Rails.env.staging?)
