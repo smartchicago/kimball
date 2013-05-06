cae1b4a2373480efe7db376de22a8a870425ad42
cgansen <cgansen@gmail.com>
2013-05-06 14:59:07 -0500

    enforce auth on all actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :authenticate_user!
+
   before_filter :janky_authentication
 
   protected
