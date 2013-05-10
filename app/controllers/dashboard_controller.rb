794016a91dfb6bd4775a94844318a1594abceed5
cgansen <cgansen@gmail.com>
2013-05-09 21:37:25 -0500

    fix deprecated finder

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,7 +4,7 @@ class DashboardController < ApplicationController
     @submissions        = Submission.order('created_at DESC').where("created_at > ?", 1.week.ago)    
     @recent_signups     = @people.size
     @recent_submissions = @submissions.size
-    @top_five_wards     = Person.count(group: :geography_id, order: 'count_all DESC', limit: 5)
-    @bottom_five_wards  = Person.count(group: :geography_id, order: 'count_all ASC', limit: 5)
+    @top_five_wards     = Person.group(:geography_id).order('count_all DESC').limit(5).count
+    @bottom_five_wards  = Person.group(:geography_id).order('count_all ASC').limit(5).count
   end
 end
