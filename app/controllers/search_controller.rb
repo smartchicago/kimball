066a6fd706c076e577b3aed43448f9b109de36b4
cgansen <cgansen@gmail.com>
2013-05-16 20:26:32 -0500

    tidying up export to mailchimp

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,7 +4,6 @@ class SearchController < ApplicationController
   def index
     # no pagination for CSV export
     per_page = request.format.to_s.eql?('text/csv') ? 10000 : Person.per_page
-    @mailchimp_export = MailchimpExport.new
     
     @results = if params[:q]
       Person.search params[:q], per_page: per_page, page: (params[:page] || 1)
@@ -19,5 +18,23 @@ class SearchController < ApplicationController
       format.csv { } 
     end
 
+  end
+
+  def export
+    # send all results to a new static segment in mailchimp
+    list_name = params.delete(:name)
+    @people = Person.complex_search(params, 10000)
+    @mce = MailchimpExport.new(name: list_name, recipients: @people.collect{ |person| person.email_address }, created_by: current_user.id)
+    
+    if @mce.save
+      Rails.logger.info("[SearchController#export] Sent #{@mce.recipients.size} email addresses to a static segment named #{@mce.name}")
+      respond_to do |format|
+        format.js { }
+      end
+    else
+      Rails.logger.error("[SearchController#export] failed to send event to mailchimp: #{@mce.errors.inspect}")
+      format.all { render text: "failed to send event to mailchimp: #{@mce.errors.inspect}", status: 400} 
+    end
+    
   end
 end
