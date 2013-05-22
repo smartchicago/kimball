0ea686f2f1df30a594e57f40fcb672cc9581ecf3
cgansen <cgansen@gmail.com>
2013-05-22 17:24:09 -0500

    adding support for searching tags

--- app/models/tagging.rb
+++ app/models/tagging.rb
@@ -1,6 +1,6 @@
 class Tagging < ActiveRecord::Base
   belongs_to :tag
-  belongs_to :taggable, polymorphic: true
+  belongs_to :taggable, polymorphic: true, touch: true
 
   attr_accessor :name
 end
