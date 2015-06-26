a4c2730ec731b73d9aaf0a3b4a17c1024c566b1d
Josh Kalov <jkalov@gmail.com>
2015-06-25 22:54:17 -0500

    fix tyo

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -32,7 +32,7 @@ class PeopleController < ApplicationController
   def create
     from_wufoo = false
     uatest = request.headers["User-Agent"]
-    if uatest = "Wufoo.com"
+    if uatest == "Wufoo.com"
       if params['HandshakeKey'].present?
         if Logan::Application.config.wufoo_handshake_key != params['HandshakeKey']
           Rails.logger.warn("[wufoo] received request with invalid handshake. Full request: #{request.inspect}")
