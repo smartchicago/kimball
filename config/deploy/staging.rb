d0acc7e675a52d40aa0efa145250a8d7cc216c08
Josh Kalov <jkalov@gmail.com>
2015-06-29 14:54:04 -0500

    update environment variables

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,7 +4,7 @@ set :rails_env, :staging
 
 server 'patterns-staging.smartchicagoapps.org', :app, :web, :db, :primary => true
 
-#task :link_env_var do
+task :link_env_var do
     # pull in database.yml on server
-#    run "rm -f #{release_path}/config/local_env.yml && ln -s #{deploy_to}/shared/local_env.yml #{release_path}/config/local_env.yml"
-#  end
+    run "rm -f #{release_path}/config/local_env.yml && ln -s #{deploy_to}/shared/local_env.yml #{release_path}/config/local_env.yml" 
+end
