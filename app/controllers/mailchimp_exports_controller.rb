fc660c00f3b25162109468e7471c8cecc28d21af
Josh Kalov <jkalov@gmail.com>
2015-07-08 13:17:07 -0500

    Add send to mailchimp on verification

--- app/controllers/mailchimp_exports_controller.rb
+++ app/controllers/mailchimp_exports_controller.rb
@@ -2,4 +2,11 @@ class MailchimpExportsController < ApplicationController
   def index
     @mailchimp_exports = MailchimpExport.all    
   end
+
+
+  def add_to_list(person)
+
+  end
+
+
 end
--- config/initializers/mailchimp.rb
+++ config/initializers/mailchimp.rb
@@ -1,2 +1,2 @@
-Gibbon.api_key = "ee75320ecd03b9150f939711e5b1560f-us5"
-Logan::Application.config.cut_group_mailchimp_list_id = "74faafe842"  # the list that we will add all static segements to
+Gibbon.api_key = ENV['MAILCHIMP_API_KEY']
+Logan::Application.config.cut_group_mailchimp_list_id = ENV['MAILCHIMP_LIST_ID'] # the list that we will add all static segements to
