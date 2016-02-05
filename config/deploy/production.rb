4c5fd2fc2f3183bbbccb79352106686355307502
Bill Cromie <bill@cromie.org>
2016-02-05 16:07:31 -0500

    using env variable for production server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,4 +2,4 @@ set :application, 'logan-production'
 set :branch, fetch(:branch, 'master')
 set :rvm_ruby_string, '2.2.4@production'   # use the same ruby as used locally for deployment
 
-server 'patterns.smartchicagoapps.org', :app, :web, :db, primary: true
+server ENV['PRODUCTION_SERVER'], :app, :web, :db, primary: true
