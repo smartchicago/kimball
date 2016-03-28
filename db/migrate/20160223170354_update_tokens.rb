511c1502703cb50dfa61e325d159447a7f3d7a3d
Josh Kalov <jkalov@gmail.com>
2016-03-28 15:00:14 -0500

    ..'

--- db/migrate/20160223170354_update_tokens.rb
+++ db/migrate/20160223170354_update_tokens.rb
@@ -2,7 +2,7 @@ class UpdateTokens < ActiveRecord::Migration
   def change
     Person.find_each do |person|
       person.regenerate_token
-      person.save!
+      person.save
     end
   end
 end
