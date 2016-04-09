28b222d553ae6d5047e02d8bbc4c74b2661494b1
Josh Kalov <jkalov@gmail.com>
2016-04-09 15:04:26 -0500

    .

--- spec/models/mailchimp_update_spec.rb
+++ spec/models/mailchimp_update_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe MailchimpUpdate, type: :model do
 
-  describe "updatePerson" do  	
+  describe "updatePerson" do
     it "creates a tag and comment for unsubscribed person" do
       
     end
