5a5418248e2e01adb9848d3269783076d619492c
Bill Cromie <bill@cromie.org>
2016-04-21 12:16:02 -0400

    adding validations for reservation to prevent double booking adding a strategy for testing new reservation validations

--- app/models/v2/reservation.rb
+++ app/models/v2/reservation.rb
@@ -7,6 +7,13 @@
 #  person_id    :integer
 #
 
+# TODO: denormalize associated objects into Reservation
+# - it's the final snapshot of all scheduling, source to be checked
+# against constantly
+# - associated objects should still be in place, for cancellations
+# - potentially storing the state of the reservation:
+#   * attended, cancelled, missed, etc.
+#   * could be where we hang comments / prepaid cards
 class V2::Reservation < ActiveRecord::Base
   self.table_name = 'v2_reservations'
 
@@ -27,24 +34,25 @@ class V2::Reservation < ActiveRecord::Base
   # these overlap validations are super tricksy.
   # do we check this here?
   # User can't book over themselves.
-  # validates 'v2_time_slots.start_time', 'v2_time_slots.end_time',
-  #   overlap: {
-  #     query_options: { includes: [:time_slot, :event] },
-  #     scope: { 'v2_events.user_id' => proc { |e| e.user_id } },
-  #     exclude_edges: %w( v2_time_slots.start_time v2_time_slots.end_time ),
-  #     message_title:  'Sorry!',
-  #     message_content: 'This time is no longer available.'
-  #   }
 
-  # # person can only have one reservation at a time.
-  # validates 'v2_time_slots.start_time', 'v2_time_slots.end_time',
-  #   overlap: {
-  #     query_options: { includes: :time_slot },
-  #     scope: { 'v2_reservations.person_id' => proc { |reservation| reservation.person_id } },
-  #     exclude_edges: %w( v2_time_slots.start_time v2_time_slots.end_time ),
-  #     message_title:  'Sorry!',
-  #     message_content: 'This time is no longer available.'
-  #   }
+  validates 'v2_time_slots.start_time', 'v2_time_slots.end_time',
+    overlap: {
+      query_options: { includes: [:time_slot, :event] },
+      scope: { 'v2_events.user_id' => proc { |e| e.user_id } },
+      exclude_edges: %w( v2_time_slots.start_time v2_time_slots.end_time ),
+      message_title:  'Sorry!',
+      message_content: 'This time is no longer available.'
+    }
+
+  # person can only have one reservation at a time.
+  validates 'v2_time_slots.start_time', 'v2_time_slots.end_time',
+    overlap: {
+      query_options: { includes: :time_slot },
+      scope: { 'v2_reservations.person_id' => proc { |reservation| reservation.person_id } },
+      exclude_edges: %w( v2_time_slots.start_time v2_time_slots.end_time ),
+      message_title:  'Sorry!',
+      message_content: 'This time is no longer available.'
+    }
 
   # not sure about all these delegations.
   delegate :start_time,  to: :time_slot, allow_nil: true
