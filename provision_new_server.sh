d7ad25f2d34b637f5592d7ec80a3fa5d0c3cbbba
Bill Cromie <bill@cromie.org>
2016-05-07 14:46:37 -0400

    oops. left some testing data in the sample.local_env.yml

--- provision_new_server.sh
+++ provision_new_server.sh
@@ -104,6 +104,7 @@ EOL
   # installing ruby and rvm
   gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
   curl -sSL https://get.rvm.io | bash -s stable
+  echo 'rvm_trust_rvmrcs_flag=1' >> ~/.rvmrc
   source /home/logan/.rvm/scripts/rvm
   rvm install 2.2.4
   rvm use 2.2.4@`echo $RAILS_ENV` --create
