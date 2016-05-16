140a9dd98613afd2a025b65ab0be4cdb03884a39
Bill Cromie <bill@cromie.org>
2016-05-16 17:52:08 -0400

    man, time zones.

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,6 +5,7 @@
 
 # loading our environment variables and defaults
 require 'yaml'
+require 'time'
 env_file = File.dirname(__FILE__) + '/local_env.yml'
 defaults = File.dirname(__FILE__) + '/sample.local_env.yml'
 
@@ -23,12 +24,20 @@ path = File.expand_path(File.dirname(File.dirname(__FILE__)))
 set :job_template, "TZ=\"#{ENV['TIME_ZONE']}\" bash -l -c ':job'"
 set :output, "#{path}/log/cron_log.log"
 #
+
 every 30.minutes do
   command "backup perform --trigger my_backup -r #{path}/Backup/"
 end
 
+# https://coderwall.com/p/ahdolq/local-timezone-fix-for-whenever-gem
+# see: https://github.com/javan/whenever/pull/239
+# time should be > 03:00
+def nyc_time(time)
+  TZInfo::Timezone.get('America/New_York').local_to_utc(Time.parse(time))
+end
+
 # this queues up all the email/sms for the day!
-every :weekday, at: "8:00am" do
+every :day, at: nyc_time("8:00am") do
   runner "User.send_all_reminders"
   runner "Person.send_all_reminders"
 end
