f7249c36050ef8359a0712516a4fa74a0734d52e
Josh Kalov <jkalov@gmail.com>
2016-05-23 11:31:59 -0500

    temporarily hide unneeded links

--- db/migrate/20160223170354_update_tokens.rb
+++ db/migrate/20160223170354_update_tokens.rb
@@ -1,8 +1,8 @@
 class UpdateTokens < ActiveRecord::Migration
   def change
-    Person.find_each do |person|
-      person.regenerate_token
-      person.save
-    end
+    # Person.find_each do |person|
+    #   person.regenerate_token
+    #   person.save
+    # end
   end
 end
