1a43ba12f8f88a9fc1b6b93a7fa82c82bbfc098b
Josh Kalov <jkalov@gmail.com>
2016-07-25 08:33:25 -0500

    sort gift card people by signup date

--- app/controllers/gift_cards_controller.rb
+++ app/controllers/gift_cards_controller.rb
@@ -5,7 +5,7 @@ class GiftCardsController < ApplicationController
   # GET /gift_cards.json
   def index
     @gift_cards = GiftCard.includes(:person).all
-    @recent_signups = Person.no_signup_card
+    @recent_signups = Person.no_signup_card.order('signup_at DESC')
     @new_gift_cards = []
     @recent_signups.length.times do
       @new_gift_cards << GiftCard.new
