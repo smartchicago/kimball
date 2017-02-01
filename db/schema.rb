272c5bbd9abe5e864a5d7639b3157994044c8bd5
Bill Cromie <bill@cromie.org>
2017-02-01 16:22:07 -0500

    changing proxy_id to a string

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160816094658) do
+ActiveRecord::Schema.define(version: 20170201205326) do
 
   create_table "applications", force: :cascade do |t|
     t.string   "name",         limit: 255
@@ -85,7 +85,7 @@ ActiveRecord::Schema.define(version: 20160816094658) do
     t.datetime "created_at",                                   null: false
     t.datetime "updated_at",                                   null: false
     t.string   "batch_id",         limit: 255
-    t.integer  "proxy_id",         limit: 4
+    t.string   "proxy_id",         limit: 255
   end
 
   add_index "gift_cards", ["giftable_type", "giftable_id"], name: "index_gift_cards_on_giftable_type_and_giftable_id", using: :btree
